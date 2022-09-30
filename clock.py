4702c3ba4dea013b445eb2b819d8aeaa0e64c914
Paul FLeming <pfleming@heroku.com>
2022-09-30 11:10:53 +0100

    Added basic DB check w updated requirements

--- clock.py
+++ clock.py
@@ -1,5 +1,6 @@
 from apscheduler.schedulers.blocking import BlockingScheduler 
-import psycopg2, os, time 
+import psycopg2
+import time 
 from datetime import datetime   
 
 sched = BlockingScheduler()
@@ -7,9 +8,9 @@ now = datetime.now()
 current_time = now.strftime("%H:%M:%S:%s")
 connection_url = "postgres://u7r96o31vp2hlk:pae99a023860272fca7f1e2af513f5611e66ce6437db3934837fe732eb9ecbc6e@ec2-54-210-170-57.compute-1.amazonaws.com:5432/dnvab2bl745rg"
 
-@sched.scheduled_job('interval', minutes=3)
-def test_job():
-    print('Test job runs every minute')
+#@sched.scheduled_job('interval', minutes=3)
+#def test_job():
+#    print('Test job runs every minute')
     
 @sched.scheduled_job('interval', seconds=15)
 def check_connection(connection_url):
