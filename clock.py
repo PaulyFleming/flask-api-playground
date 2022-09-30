e6aca30b900bfe47eb1c8eb39d7d891ba9943cd2
Paul FLeming <pfleming@heroku.com>
2022-09-30 11:46:40 +0100

    update clock

--- clock.py
+++ clock.py
@@ -8,9 +8,9 @@ now = datetime.now()
 current_time = now.strftime("%H:%M:%S:%s")
 connection_url = "postgres://u7r96o31vp2hlk:pae99a023860272fca7f1e2af513f5611e66ce6437db3934837fe732eb9ecbc6e@ec2-54-210-170-57.compute-1.amazonaws.com:5432/dnvab2bl745rg"
 
-@sched.scheduled_job('interval', minutes=3)
+@sched.scheduled_job('interval', minutes=2)
 def test_job():
-    print('Test job runs every minute')
+    print('Test job runs every 2 minutes')
     
 @sched.scheduled_job('interval', seconds=15)
 def check_connection():
@@ -18,15 +18,15 @@ def check_connection():
     #connection_url= connection_url
     try:
         conn = psycopg2.connect(connection_url)
+        
         print(f"Connection to {name} succesful at ", current_time)
     except:
-        print("Connection failure at ", current_time)
+        print(f"Connection to {name} failure at ", current_time)
         
-    for x in range(5): 
-        time.sleep(0.5)
-        print("...")
+    print(conn.closed)
+        
 
-    conn.close()
+    #conn.close()
     print("Connection closed at ", current_time)
         
 
